6129563fed19fac8ca7fab76c1918fe286d3338c
Joaquin Farah <Joaquin Farah@DESKTOP-320FN8B>
2022-07-28 21:03:35 -0300

    agregado el jquery y autocompletado

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -11,6 +11,10 @@ namespace SchoolManagment
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -24,7 +28,12 @@ namespace SchoolManagment
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css", 
+                      "~/Content/themes/base/autocomplete.css"));
+
+            bundles.Add(new StyleBundle("~/Content/jqueryui").Include(
+                      "~/Content/theme/base/all.css",
+                      "~/Content/themes/base/autocomplete.css"));
         }
     }
 }
